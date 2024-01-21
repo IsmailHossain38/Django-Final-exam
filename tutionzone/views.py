64550acd1e4f9768b38d5a0c7fcc902149826298
IsmailHossain38 <ismailhossains383@gmail.com>
2024-01-21 21:31:51 +0600

    design changed

--- tutionzone/views.py
+++ tutionzone/views.py
@@ -5,6 +5,7 @@ from django.shortcuts import render
 def home(request ,Category_slug =None):
     data = AddTutors.objects.all()
     form = CategoryByClasses.objects.all()
+ 
     if Category_slug is not None:
         class_category = CategoryByClasses.objects.get(slug = Category_slug)
         data = AddTutors.objects.filter(class_category=class_category)
